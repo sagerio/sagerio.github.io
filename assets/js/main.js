30383e32c5b3dc02dacecb8e505783d9af3425d4
sager.io <robert@sager.io>
2024-08-05 16:42:53 +0200

    updated all

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,198 +1,137 @@
 
-function getRandomFontSize() {
-	// return `${Math.random() + 2}rem`;
-	const val = ((Math.random() * 1.5) + 1).toFixed(1);
-	// console.log(val);
-	return `${val}rem`;
+// function getRandomFontSize() {
+// 	const rand = (Math.random() * (1.3 - 0.7) + 0.7).toFixed(1);
+// 	console.log(rand);
+// 	return `${rand}em`;
+// }
+
+let isRunning = false;
+
+const wait = async ms => new Promise(resolve => setTimeout(resolve, ms));
+
+async function showSkills() {
+	if (window.location.hash === "#work" && !isRunning) {
+		const typewriter = document.getElementById("typewriter")
+		document.querySelectorAll("div#typewriter span.wrd").forEach(e => e.remove());
+		if (typewriter) {
+			isRunning = true;
+			for (const s of strings) {
+				const span = document.createElement("span");
+				span.classList.add("wrd");
+				// span.style.fontSize = getRandomFontSize();
+				typewriter.appendChild(span);
+				for (const c of Array.from(s)) {
+					await wait(50);
+					span.textContent += c;
+				}
+				span.classList.add("border");
+				await wait(250);
+			}
+			isRunning = false;
+		}
+	}
 }
 
 
-(function ($) {
+const strings = [ ".NET", "Angular", "Apache", "Bash", "Bootstrap", "C#", "CSS3", "Docker", "Electron",
+"Entity Framework", "Express", "Git", "Gitlab", "GraphQL", "HTML5", "Hyper-V", "IntelliJ Idea", "Java",
+"Javascript", "Jenkins", "Jest", "JQuery", "JSON", "Kanban", "LDAP", "LINQ", "Linux", "LVM", "MariaDB", "MongoDB",
+"MSSQL", "MySQL", "NestJS", "Nginx", "NoSQL", "Office", "PGP", "PHP", "Powershell", "Prisma", "Python", "RegEx",
+"RestAPI", "SCRUM", "SCSS", "SQLite", "TFS", "TypeORM", "Typescript", "Unity", "Unit Testing", "Virtual Box",
+"Visual Studio", "Visual Studio Code", "vSphere", "Windows", "XML"];
 
-	var $window = $(window),
-		$body = $('body'),
-		$wrapper = $('#wrapper'),
-		$main = $('#main'),
-		$panels = $main.children('.panel'),
-		$nav = $('#nav'), $nav_links = $nav.children('a'),
-		$htmltagcloud = $('#htmltagcloud');
-	$words = $htmltagcloud.children('.wrd');
-
-	// Breakpoints.
-	breakpoints({
-		xlarge: [ '1281px', '1680px' ],
-		large: [ '981px', '1280px' ],
-		medium: [ '737px', '980px' ],
-		small: [ '361px', '736px' ],
-		xsmall: [ null, '360px' ]
-	});
+($ => {
+	const $window = $(window);
+	const $body = $("body");
+	const $nav = $("#nav");
+	const $nav_links = $nav.children("a");
+	const $main = $("#main");
+	const $panels = $main.children(".panel");
 
 	// Play initial animations on page load.
-	$window.on('load', function () {
-		$words.each(function () {
-			$(this).css('font-size', getRandomFontSize());
-		});
-		window.setTimeout(function () {
-			$body.removeClass('is-preload');
-		}, 250);
+	$window.on("load", () => {
+		window.setTimeout(() => {
+			$("#year").html(new Date().getFullYear());
+			$body.removeClass("preload");
+		}, 500);
 	});
 
-	// Nav.
-	$nav_links.on('click', function (event) {
-
-		var href = $(this).attr('href');
-
-		// Not a panel link? Bail.
-		if (href.charAt(0) != '#') //|| $panels.filter(href).length == 0)
-			return;
-
-		// Prevent default.
-		event.preventDefault();
-		event.stopPropagation();
-
-		// Change panels.
-		if (window.location.hash != href)
-			window.location.hash = href;
-	});
-
-	// Panels.
 
 	// Initialize.
-	(function () {
-
-		var $panel, $link;
-
+	(() => {
+		let $panel;
+		let $link;
 		// Get panel, link.
 		if (window.location.hash) {
-
 			$panel = $panels.filter(window.location.hash);
-			$link = $nav_links.filter('[href="' + window.location.hash + '"]');
-
+			$link = $nav_links.filter(`[href="${window.location.hash}"]`);
 		}
-
 		// No panel/link? Default to first.
-		if (!$panel || $panel.length == 0) {
+		if (!$panel || $panel.length === 0) {
 			$panel = $panels.first();
 			$link = $nav_links.first();
 		}
-
 		// Deactivate all panels except this one.
-		$panels.not($panel)
-			.addClass('inactive')
-			.hide();
-
+		$panels.not($panel).addClass("inactive").hide();
 		// Activate link.
-		$link.addClass('active');
-
+		$link.addClass("active");
 		// Reset scroll.
 		$window.scrollTop(0);
+		showSkills();
 	})();
 
-	// Hashchange event.
-	$window.on('hashchange', function (event) {
 
-		var $panel, $link;
+	// Hashchange event.
+	$window.on("hashchange", () => {
+
+		let $panel;
+		let $link;
 
 		// Get panel, link.
 		if (window.location.hash) {
 			$panel = $panels.filter(window.location.hash);
-			$link = $nav_links.filter('[href="' + window.location.hash + '"]');
+			$link = $nav_links.filter(`[href="${window.location.hash}"]`);
 			// No target panel? Bail.
-			if ($panel.length == 0)
+			if ($panel.length === 0) {
 				return;
-		}
-		// No panel/link? Default to first.
-		else {
+			}
+		} else {
+			// No panel/link? Default to first.
 			$panel = $panels.first();
 			$link = $nav_links.first();
 		}
 
 		// Deactivate all panels.
-		$panels.addClass('inactive');
+		$panels.addClass("inactive");
 
 		// Deactivate all links.
-		$nav_links.removeClass('active');
+		$nav_links.removeClass("active");
 
 		// Activate target link.
-		$link.addClass('active');
+		$link.addClass("active");
 
 		// Set max/min height.
-		$main
-			.css('max-height', $main.height() + 'px')
-			.css('min-height', $main.height() + 'px');
+		$main.css("max-height", `${$main.height()}px`).css("min-height", `${$main.height()}px`);
 
 		// Delay.
-		setTimeout(function () {
-
+		setTimeout(async () => {
 			// Hide all panels.
 			$panels.hide();
-
 			// Show target panel.
 			$panel.show();
-
 			// Set new max/min height.
-			$main
-				.css('max-height', $panel.outerHeight() + 'px')
-				.css('min-height', $panel.outerHeight() + 'px');
-
+			$main.css("max-height", `${$panel.outerHeight()}px`).css("min-height", `${$panel.outerHeight()}px`);
 			// Reset scroll.
 			$window.scrollTop(0);
-
 			// Delay.
-			window.setTimeout(function () {
-
-				// Activate target panel.
-				$panel.removeClass('inactive');
-
+			window.setTimeout(() => {
+				$panel.removeClass("inactive");
 				// Clear max/min height.
-				$main
-					.css('max-height', '')
-					.css('min-height', '');
-
-				// IE: Refresh.
-				$window.triggerHandler('--refresh');
-
-				// Unlock.
-				locked = false;
-
-			}, (breakpoints.active('small') ? 0 : 500));
-
+				$main.css("max-height", "").css("min-height", "");
+				showSkills();
+			}, 500);
 		}, 250);
 
 	});
-
-	// IE: Fixes.
-	if (browser.name == 'ie') {
-
-		// Fix min-height/flexbox.
-		$window.on('--refresh', function () {
-			$wrapper.css('height', 'auto');
-
-			window.setTimeout(function () {
-				var h = $wrapper.height(),
-					wh = $window.height();
-				if (h < wh) {
-					$wrapper.css('height', '100vh');
-				}
-			}, 0);
-		});
-
-		$window.on('resize load', function () {
-			$window.triggerHandler('--refresh');
-		});
-
-		// Fix intro pic.
-		$('.panel.intro').each(function () {
-			var $pic = $(this).children('.pic'),
-				$img = $pic.children('img');
-
-			$pic.css('background-image', 'url(' + $img.attr('src') + ')')
-				.css('background-size', 'cover')
-				.css('background-position', 'center');
-
-			$img.css('visibility', 'hidden');
-		});
-
-	}
 
 })(jQuery);
